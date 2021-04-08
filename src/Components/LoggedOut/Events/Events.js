97ee9efeda396fd3d6ff0810814814ebd066156c
cognitive-ninja <saurabhhirugade@gmail.com>
2021-04-08 19:32:46 +0530

    Events Component Added

---
+++ src/Components/LoggedOut/Events/Events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Events(){
+    return(
+        <>
+            <div style={{ fontFamily:"Montserrat", fontSize:"Bold", height:"70vh", textAlign:"center", padding:"70px 0"}}>
+                EVENTS WILL BE DISPLAYED HERE...
+            </div>
+        </>
+    );
+}
