264d1fa905e4a0cb3496e384c4f0595804a2784d
cognitive-ninja <saurabhhirugade@gmail.com>
2021-04-08 19:33:19 +0530

    Events Component Added

--- src/Components/LoggedOut/LoggedOut.js
+++ src/Components/LoggedOut/LoggedOut.js
@@ -5,6 +5,7 @@ import Register from './Register/Register';
 import Navigation from './Navigation/Navigation';
 import Footer from './Footer/Footer';
 import {Route, BrowserRouter as Router} from "react-router-dom";
+import Events from './Events/Events';
 
 export default function LoggedOut(){
     return(
@@ -13,6 +14,7 @@ export default function LoggedOut(){
                 {console.log("In LoggedOut")}
                 <Navigation/>
                 <Route exact path="/home" component={Home}/>
+                <Route exact path="/events" component={Events}/>
                 <Route exact path="/login" component={Login}/>
                 <Route exact path="/register" component={Register}/>
                 <Footer/>
