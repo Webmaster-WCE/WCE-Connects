47adc7141ff169af5c25facb9adfd4471ead3bee
cognitive-ninja <saurabhhirugade@gmail.com>
2021-05-28 11:40:55 +0530

    Footer Adjusted

--- src/Components/LoggedOut/Home/Home.js
+++ src/Components/LoggedOut/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import pic from './wce_pic.jpg'
+import pic from './wce_pic.jpg';
+import Footer from '../Footer/Footer';
+
 export default function Home(){
     return(
         <>
             <div style={{backgroundColor:"black", color:"white"}}>
                 <img src={pic} alt="pic" style={{minWidth:"100%", height:"80%"}}/>
             </div>
+            <Footer/>
         </>
     );
 }
--- src/Components/LoggedOut/Register/EmailVerifation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const EmailVerifation = () => {
-    return (
-        <div style={{ fontFamily:"Montserrat", fontSize:"40px", height:"70vh", textAlign:"center", padding:"70px 0"}}>
-            We have sent you a verfication email.<br/>Please check your inbox.
-        </div>
-    )
-}
