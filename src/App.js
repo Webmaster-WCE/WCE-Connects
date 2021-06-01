d74bfedf1f62ab40ecf3c8c3752013ec3a46a29d
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-01 18:21:05 +0530

    Added Routing here

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
+import React, {useContext} from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import LoggedOut from './Components/LoggedOut/LoggedOut';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { LoggedIn } from './Components/LoggedIn/LoggedIn';
+import { VerifyEmail } from './Components/LoggedOut/Register/VerifyEmail';
+import EmailVerified from './Components/LoggedOut/Register/EmailVerified';
+import Home from './Components/LoggedOut/Home/Home';
+import Login from './Components/LoggedOut/Login/Login';
+import Register from './Components/LoggedOut/Register/Register';
+import Navigation from './Components/LoggedOut/Navigation/Navigation';
+import Events from './Components/LoggedOut/Events/Events';
+import {AuthContext} from './context/AuthContext';
 
 function App() {
+  const {token} = useContext(AuthContext);
+
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
+        {token !== null? <Redirect to="/u/landing"/> : 
         <Switch>
+            <Route exact path="/">
+              <Navigation/>
+              <Home/>
+            </Route>
+            <Route exact path="/events">
+              <Navigation/>
+              <Events/>
+            </Route>
+            <Route exact path='/login'>
+              <Navigation/>
+              <Login/>
+            </Route>
+            <Route exact path="/register">
+              <Navigation/>
+              <Register/>
+            </Route>
+            <Route exact path="/verify">
+              <VerifyEmail/>
+            </Route>
+            <Route exact path="/verified">
+              <EmailVerified/>
+            </Route>
+        </Switch>}
         <Route path="/u"> 
-            <LoggedIn/>
-          </Route>
-          <Route path="/">
-            <LoggedOut/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+          <LoggedIn/>
+        </Route>
+          
+      </Router>
     </div>
   );
 }
