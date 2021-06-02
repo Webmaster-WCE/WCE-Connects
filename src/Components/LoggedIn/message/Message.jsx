bf2d3d2b63a58e5ae7b311251861c7983d1c9cd7
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-02 19:30:08 +0530

    Added default propic

--- src/Components/LoggedIn/message/Message.jsx
+++ src/Components/LoggedIn/message/Message.jsx
@@ -7,7 +7,7 @@ export default function Message({ message, own }) {
       <div className="messageTop">
         <img
           className="messageImg"
-          src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src="https://lh3.googleusercontent.com/JW3y8hF1MZHf1_u6BGpd8Km8Bhqmk648mIH2UnZFkyQHnlBzC7Ilj6VBluG0DiC344gjAHSr1tGX8zsLWaqB=w1920-h902-rw"
           alt=""
         />
         <p className="messageText">{message.text}</p>
