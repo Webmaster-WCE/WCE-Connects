0fd54f0551dc2aaea736f4533329da0624c29971
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-04 10:10:59 +0530

    Added new image path

--- src/Components/LoggedOut/Home/Home.js
+++ src/Components/LoggedOut/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import pic from './wce_pic.jpg';
 import Footer from '../Footer/Footer';
 
 export default function Home(){
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     return(
         <>
             <div style={{backgroundColor:"black", color:"white"}}>
-                <img src={pic} alt="pic" style={{minWidth:"100%", height:"80%"}}/>
+                <img src={PF+"wce_pic.jpg"} alt="pic" style={{minWidth:"100%", height:"80%"}}/>
             </div>
             <Footer/>
         </>
