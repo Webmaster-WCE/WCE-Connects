0975296bbbfe852b20e26a106d5bc0b7de4030bf
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-08 12:27:38 +0530

    Fetched Feed and Notification components

--- src/Components/LoggedIn/Landing/Landing.js
+++ src/Components/LoggedIn/Landing/Landing.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Grid, Paper } from "@material-ui/core";
 import MinProfile from './MinProfile';
 import CreatePost from './CreatePost';
+import Feed from '../Feed/Feed';
+import Notification from '../Notification/Notification';
+// import Events from '../../LoggedOut/Events/Events';
 
 export const Landing = () => {
     return (
@@ -20,13 +23,20 @@ export const Landing = () => {
                     <Paper style={{ paddingBottom: "5%", background: "lightgrey" }} >
                         <CreatePost/>
                     </Paper>
+                    <Feed />
                 </Grid>
                 <Grid item container direction="column" xs={12} sm={3} spacing={2}>
                     <Grid item>
-                    {/* <Paper style={{ height: "29vh", background: "orange" }} /> */}
+                    <Paper style={{ height: "31vh", background: "lightgrey", position:"fixed" , marginTop: "5%"}} >
+                        <div style={{ padding:"5%"}}>
+                         <Notification/>
+                        </div>
+                    </Paper>
                     </Grid>
                     <Grid item>
-                    {/* <Paper style={{ height: "49vh", background: "green" }} /> */}
+                    {/* <Paper style={{ height: "49vh", background: "lightgrey" }} >
+                        <Events/>
+                    </Paper> */}
                     </Grid>
                 </Grid>
             </Grid>
