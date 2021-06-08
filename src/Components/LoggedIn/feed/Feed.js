34c551bc971e8022c0ceaa2f3231471748736e83
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-08 12:26:15 +0530

    Fetching posts in feed

--- src/Components/LoggedIn/feed/Feed.js
+++ src/Components/LoggedIn/feed/Feed.js
@@ -1,19 +1,19 @@
 import { useContext, useEffect, useState } from "react";
 import Post from "./Post/Post";
-// import Share from "../share/Share";
 import "./Feed.css";
 import axios from "axios";
 import { AuthContext } from "../../../context/AuthContext";
 
-export default function Feed({ username }) {
+export default function Feed() {
   const [posts, setPosts] = useState([]);
-  const { user } = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = username
-        ? await axios.get("/posts/profile/" + username)
-        : await axios.get("posts/timeline/" + user._id);
+      const res = token
+        ? await axios.get("http://localhost:5000/feeds/all/1", {headers:{ 'x-auth-token':token }})
+        : null;
+        
       setPosts(
         res.data.sort((p1, p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
@@ -21,15 +21,16 @@ export default function Feed({ username }) {
       );
     };
     fetchPosts();
-  }, [username, user._id]);
+  }, [token]);
 
   return (
     <div className="feed">
       <div className="feedWrapper">
         {/* {(!username || username === user.username) && <Share />} */}
-        {/* {posts.map((p) => (
+        {console.log(posts)}
+        {posts.map((p) => (
           <Post key={p._id} post={p} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
--- src/Components/LoggedIn/feed/post/Post.js
+++ src/Components/LoggedIn/feed/post/Post.js
@@ -1,29 +1,28 @@
-// import "./post.css";
-// import { MoreVert } from "@material-ui/icons";
-// import { useContext, useEffect, useState } from "react";
-// import axios from "axios";
-// // import { format } from "timeago.js";
-// import { Link } from "react-router-dom";
-// import { AuthContext } from "../../../../context/AuthContext";
+import "./Post.css";
+import { MoreVert } from "@material-ui/icons";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+// import { format } from "timeago.js";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../../context/AuthContext";
 
-// export default function Post({ post }) {
-//   const [like, setLike] = useState(post.likes.length);
-//   const [isLiked, setIsLiked] = useState(false);
-//   const [user, setUser] = useState({});
-//   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-//   const { user: currentUser } = useContext(AuthContext);
+export default function Post({ post }) {
+  
+    const [user, setUser] = useState({});
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { token } = useContext(AuthContext);
 
 //   useEffect(() => {
 //     setIsLiked(post.likes.includes(currentUser._id));
 //   }, [currentUser._id, post.likes]);
 
-//   useEffect(() => {
-//     const fetchUser = async () => {
-//       const res = await axios.get(`/users?userId=${post.userId}`);
-//       setUser(res.data);
-//     };
-//     fetchUser();
-//   }, [post.userId]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`http://localhost:5000/users/${post.author}`, {headers:{'x-auth-token':token}});
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [post.author, token]);
 
 //   const likeHandler = () => {
 //     try {
@@ -32,54 +31,55 @@
 //     setLike(isLiked ? like - 1 : like + 1);
 //     setIsLiked(!isLiked);
 //   };
-//   return (
-//     <div className="post">
-//       <div className="postWrapper">
-//         <div className="postTop">
-//           <div className="postTopLeft">
-//             <Link to={`/profile/${user.username}`}>
-//               <img
-//                 className="postProfileImg"
-//                 src={
-//                   user.profilePicture
-//                     ? PF + user.profilePicture
-//                     : PF + "person/noAvatar.png"
-//                 }
-//                 alt=""
-//               />
-//             </Link>
-//             <span className="postUsername">{user.username}</span>
-//             {/* <span className="postDate">{format(post.createdAt)}</span> */}
-//           </div>
-//           <div className="postTopRight">
-//             <MoreVert />
-//           </div>
-//         </div>
-//         <div className="postCenter">
-//           <span className="postText">{post?.desc}</span>
-//           <img className="postImg" src={PF + post.img} alt="" />
-//         </div>
-//         <div className="postBottom">
-//           <div className="postBottomLeft">
-//             <img
-//               className="likeIcon"
-//               src={`${PF}like.png`}
-//               onClick={likeHandler}
-//               alt=""
-//             />
-//             <img
-//               className="likeIcon"
-//               src={`${PF}heart.png`}
-//               onClick={likeHandler}
-//               alt=""
-//             />
-//             <span className="postLikeCounter">{like} people like it</span>
-//           </div>
-//           <div className="postBottomRight">
-//             <span className="postCommentText">{post.comment} comments</span>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+      <div className="post">
+        {console.log("post"+ post.author)}
+      { user && user.info ? <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <Link to={`/profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                src={
+                  user.info.profilePicture
+                    ? user.info.profilePicture
+                    : PF + "default.png"
+                }
+                alt=""
+              />
+            </Link>
+            <span className="postUsername">{user.info.first_name} {user.info.last_name}</span>
+            {/* <span className="postDate">{format(post.createdAt)}</span> */}
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">{post?.text}</span>
+          <img className="postImg" src={post.picture} alt="" />
+        </div>
+        {/* <div className="postBottom">
+          <div className="postBottomLeft">
+            <img
+              className="likeIcon"
+              src={`${PF}like.png`}
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src={`${PF}heart.png`}
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCounter">{like} people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div> */}
+      </div>  : null}
+    </div>
+  );
+}
