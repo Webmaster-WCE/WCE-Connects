782c415e25937347a0368ce760dd82950c586598
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-08 12:28:28 +0530

    Fetching Profile, Integration with backend

--- src/Components/LoggedIn/profile/ProfilePage.js
+++ src/Components/LoggedIn/profile/ProfilePage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useContext, useState, useEffect} from 'react';
+import axios from 'axios';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
@@ -15,57 +16,11 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import {AuthContext} from '../../../context/AuthContext';
 
-const user = {
-    short_profile: {
-        first_name: 'Saurabh',
-        last_name: 'Hirugade',
-        post:'Student',
-        organization:'Walchand College of Engineering, Sangli',
-    },
-    professional_info: [
-        {
-            post: 'Program Director',
-            company: 'WCE ACM Student Chapter',
-            from:'April 2019',
-            to: 'Present'
-        },
-    ],
-    educational_info:[
-        {
-            school:'Walchand College of Engineering, Sangli',
-            course:'B.Tech in Computer Science and Engineering',
-            score:'8.37',
-            outof:'10',
-        },
-        {
-            
-            school:'Swami Vivekanand College, Kolhapur',
-            course:'HSC (Science)',
-            score:'89.85',
-            outof:'100'
-        },
-
-    ],
-    personal_info: {
-        about_me: "Hello, I'm Pre-Final Year Computer Science And Enigneering Student. I love development and problem solving.",
-        birthdate:'11/09/2000',
-    },
-    contact_info: {
-        mobile_no: '7769983805',
-        email: 'saurabhhirugade@gmail.com',
-    },
-    profiles: {
-        linkedin: 'https://www.linkedin.com/in/saurabh-hirugade/',
-        github: 'https://github.com/cognitive-ninja',
-        twitter: 'https://twitter.com/DarkEnergy__',
-        youtube: '',
-        instagram: '',
-        facebook: '',
-    }
-}
 const useStyles = makeStyles((theme) => ({
     short_profile: {
+        marginTop:"5%",
         padding: '30px',
         display: 'flex',
         flexDirection: 'row',
@@ -127,51 +82,90 @@ export default function ProfilePage() {
     
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(true);
+    const [userProfile, setUserProfile] = useState();
+    const [user, setUser] = useState();
+    const [isLoading, setIsLoading] = useState(false);
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-    
+    const {token} = useContext(AuthContext);
+
     const toggleExpanded = () => {
         setExpanded(!expanded);
     };
     
+    useEffect(() => {
+        const fetchUser = async () => {
+            setIsLoading(true);
+            try{
+                const res = await axios.get(`http://localhost:5000/users/current`, 
+                    { headers: {'x-auth-token':token}
+                });
+                setUser(res.data);
+            }
+            catch(err){
+                console.log(err);
+            }
+            setIsLoading(false);
+        }
+        fetchUser();
+    },[token]);
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            setIsLoading(true);
+            try{
+                const res = await axios.get(`http://localhost:5000/userProfile/current`, 
+                    { headers: {'x-auth-token':token}
+                });
+                setUserProfile(res.data);
+            }
+            catch(err){
+                console.log(err);
+            }
+            setIsLoading(false);
+        }
+        fetchUserProfile();
+    },[token]);
+
     return (
         <>
-            <div className={classes.short_profile}>
+            {isLoading ? console.log("Loading in progress...") : null}
+            {user? <div className={classes.short_profile}>
                 <Avatar alt="Profile Photo" className={classes.propic} src={PF+"default.png"} />
                 <div className={classes.basic_info}>
                     <div className={classes.short_profile_heading}>
-                        <p style={{fontFamily:"Montserrat", fontSize:"40px", margin: "0px" }}>{user.short_profile.first_name} {user.short_profile.last_name}</p>
+                        <p style={{fontFamily:"Montserrat", fontSize:"40px", margin: "0px" }}>{user.info.first_name} {user.info.last_name}</p>
                         <a href="/u/profile/userid/edit" style={{textDecoration:"none"}}><Button variant="contained">Edit Profile</Button></a>
                     </div>
                     <div className={classes.short_profile_subheading}>
-                        <p style={{ fontSize:"large",   margin:"10px 0px 0px 0px" }}>{user.short_profile.post}</p>
-                        <p style={{fontSize:"x-large",  margin:"0px" }}>{user.short_profile.organization}</p>
+                        <p style={{ fontSize:"large",   margin:"10px 0px 0px 0px" }}>{user.info.current_post}</p>
+                        <p style={{fontSize:"x-large",  margin:"0px" }}>{user.info.current_organization}</p>
                     </div>
                 </div>
-            </div>
+            </div> : null}
         
 
-            <Grid container justify="center" direction="row">
-                {user.profiles.linkedin && <IconButton href={user.profiles.linkedin} target="_blank" rel="noreferrer">
+            {userProfile ? <Grid container justify="center" direction="row">
+                {userProfile.profiles.linkedin && <IconButton href={userProfile.profiles.linkedin} target="_blank" rel="noreferrer">
                     <LinkedInIcon className={classes.profileIcons}/>
                 </IconButton>}
-                {user.profiles.github && <IconButton href={user.profiles.github} target="_blank" rel="noreferrer">
+                {userProfile.profiles.github && <IconButton href={userProfile.profiles.github} target="_blank" rel="noreferrer">
                     <GitHubIcon className={classes.profileIcons}/>
                 </IconButton>}
-                {user.profiles.twitter && <IconButton href={"https://twitter.com/"+user.profiles.twitter} target="_blank" rel="noreferrer">
+                {userProfile.profiles.twitter && <IconButton href={"https://twitter.com/"+userProfile.profiles.twitter} target="_blank" rel="noreferrer">
                     <TwitterIcon className={classes.profileIcons}/>
                 </IconButton>}
-                {user.profiles.youtube && <IconButton href={user.profiles.youtube} target="_blank" rel="noreferrer">
+                {userProfile.profiles.youtube && <IconButton href={userProfile.profiles.youtube} target="_blank" rel="noreferrer">
                     <YouTubeIcon className={classes.profileIcons}/>
                 </IconButton>}
-                {user.profiles.facebook && <IconButton href={user.profiles.facebook} target="_blank" rel="noreferrer">
+                {userProfile.profiles.facebook && <IconButton href={userProfile.profiles.facebook} target="_blank" rel="noreferrer">
                     <FacebookIcon className={classes.profileIcons}/>
                 </IconButton>}
-                {user.profiles.instagram && <IconButton href={"https://www.instagram.com/"+user.profiles.instagram} target="_blank" rel="noreferrer">
+                {userProfile.profiles.instagram && <IconButton href={"https://www.instagram.com/"+userProfile.profiles.instagram} target="_blank" rel="noreferrer">
                     <InstagramIcon className={classes.profileIcons}/>
                 </IconButton>}
-            </Grid>
+            </Grid> : null}
 
-            <div className={classes.root}>
+            {userProfile ? <div className={classes.root}>
                 <Accordion className={classes.accordionItem} expanded={expanded} onChange={toggleExpanded}>
                     <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
@@ -187,15 +181,15 @@ export default function ProfilePage() {
                             <BusinessIcon />
                             <div style={{padding:"0px 10px"}}>
                                 <div style={{fontWeight: "700"}}>
-                                    {user.professional_info[0].post}
+                                    {userProfile.professional_info[0].post}
                                 </div>  
                                 {/* <br/> */}
                                 <div style={{fontWeight: "500"}}>
-                                    {user.professional_info[0].company}
+                                    {userProfile.professional_info[0].company}
                                 </div>
                                 {/* <br/>   */}
                                 <div style={{fontSize: "small"}}>
-                                    {user.professional_info[0].from}-{user.professional_info[0].to}
+                                    {userProfile.professional_info[0].from}-{userProfile.professional_info[0].to}
                                 </div>  
                             </div>
                         </div>
@@ -214,13 +208,13 @@ export default function ProfilePage() {
                             <SchoolRoundedIcon />
                             <div style={{padding:"0px 10px"}}>
                                 <div style={{fontWeight: "700"}}>
-                                    {user.educational_info[0].school}
+                                    {userProfile.educational_info[0].school}
                                 </div>  
                                 <div style={{fontWeight: "500"}}>
-                                    {user.educational_info[0].course}
+                                    {userProfile.educational_info[0].course}
                                 </div>
                                 <div style={{fontSize: "small"}}>
-                                    {user.educational_info[0].score}/{user.educational_info[0].outof}
+                                    {userProfile.educational_info[0].score}/{userProfile.educational_info[0].outof}
                                 </div>  
                             </div>
                         </div>
@@ -237,9 +231,9 @@ export default function ProfilePage() {
                     <AccordionDetails>
                     
                         <div style={{display: 'flex', flexDirection: 'column', padding:"2px"}}>
-                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>About me:</p> <p style={{ margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{user.personal_info.about_me}</p></div>
+                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>About me:</p> <p style={{ margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{userProfile.personal_info.about_me}</p></div>
                             <hr/>
-                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>Birth-Date:</p> <p style={{margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{user.personal_info.birthdate}</p></div>
+                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>Birth-Date:</p> <p style={{margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{userProfile.personal_info.birthdate}</p></div>
                         </div>
 
                     </AccordionDetails>
@@ -254,13 +248,65 @@ export default function ProfilePage() {
                     </AccordionSummary>
                     <AccordionDetails>
                         <div style={{display: 'flex', flexDirection: 'column', padding:"2px"}}>
-                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>Email:</p> <p style={{ margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{user.contact_info.email}</p></div>
+                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>Email:</p> <p style={{ margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{userProfile.contact_info.email}</p></div>
                             <hr/>
-                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>Mobile No:</p> <p style={{margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{user.contact_info.mobile_no}</p></div>
+                            <div style={{display:"flex", padding:"4px"}}><p className={classes.subheading}>Mobile No:</p> <p style={{margin:"0px", padding:"2px 10px 2px 10px",textAlign: 'left', fontFamily:"Montserrat", fontWeight:"300"}}>{userProfile.contact_info.mobile_no}</p></div>
                         </div>
                     </AccordionDetails>
                 </Accordion>
-            </div>
+            </div> : null}
         </>
     );
 }
+
+
+//dummy_data in case if needed
+
+// const user = {
+//     short_profile: {
+//         first_name: 'Saurabh',
+//         last_name: 'Hirugade',
+//         post:'Student',
+//         organization:'Walchand College of Engineering, Sangli',
+//     },
+//     professional_info: [
+//         {
+//             post: 'Program Director',
+//             company: 'WCE ACM Student Chapter',
+//             from:'April 2019',
+//             to: 'Present'
+//         },
+//     ],
+//     educational_info:[
+//         {
+//             school:'Walchand College of Engineering, Sangli',
+//             course:'B.Tech in Computer Science and Engineering',
+//             score:'8.37',
+//             outof:'10',
+//         },
+//         {
+            
+//             school:'Swami Vivekanand College, Kolhapur',
+//             course:'HSC (Science)',
+//             score:'89.85',
+//             outof:'100'
+//         },
+
+//     ],
+//     personal_info: {
+//         about_me: "Hello, I'm Pre-Final Year Computer Science And Enigneering Student. I love development and problem solving.",
+//         birthdate:'11/09/2000',
+//     },
+//     contact_info: {
+//         mobile_no: '7769983805',
+//         email: 'saurabhhirugade@gmail.com',
+//     },
+//     profiles: {
+//         linkedin: 'https://www.linkedin.com/in/saurabh-hirugade/',
+//         github: 'https://github.com/cognitive-ninja',
+//         twitter: 'https://twitter.com/DarkEnergy__',
+//         youtube: '',
+//         instagram: '',
+//         facebook: '',
+//     }
+// }
