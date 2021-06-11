1bc96b9eb635a9e82efefc24d6d234c0ccc99e3c
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-11 12:35:29 +0530

    Footer Updated

--- src/Components/LoggedOut/Footer/Footer.js
+++ src/Components/LoggedOut/Footer/Footer.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function Footer(){
     return(
         <>
-            <div style={{ backgroundColor:"black", color:"white", height:"20vh" }}>
-                Footer
+            <div style={{ backgroundColor:"black", color:"lightgrey",fontFamily:"Montserrat", display:"flex", justifyContent:"space-around", padding:"10px" }}>
+                Developers: 
+                <a href="http://www.linkedin.com/in/rushikesh-shelke" style={{textDecoration:"none", color:"white"}}>Rushikesh Shelke</a>
+                <a href="https://www.linkedin.com/in/saurabh-hirugade/" style={{textDecoration:"none", color:"white"}}>Saurabh Hirugade</a>
+                <a href="https://www.linkedin.com/in/aryan-mali-22a501172/" style={{textDecoration:"none", color:"white"}}>Aryan Mali</a>
             </div>
         </>
     );
