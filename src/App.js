2ecd7c8215324951b9a849ffae7fe0e7657aab12
cognitive-ninja <saurabhhirugade@gmail.com>
2021-11-23 09:34:56 +0530

    Addition of HashRouter & new routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import { LoggedIn } from './components/LoggedIn/LoggedIn';
+import { HashRouter, Route, Switch } from "react-router-dom";
+// import { LoggedIn } from './components/LoggedIn/LoggedIn';
 import { VerifyEmail } from './components/LoggedOut/Register/VerifyEmail';
 import EmailVerified from './components/LoggedOut/Register/EmailVerified';
 import Home from './components/LoggedOut/Home/Home';
@@ -12,47 +12,46 @@ import Events from './components/LoggedOut/Events/Events';
 import EventPage from './components/LoggedOut/Events/EventPage';
 import EventRegister from './components/LoggedOut/Events/EventRegister';
 import Activities from './components/LoggedOut/Activities/Activities';
-import {AuthContext} from './context/AuthContext';
+import ActivityForm from './components/LoggedOut/Activities/ActivityForm';
+// import {AuthContext} from './context/AuthContext';
 import GalleryPage from './components/LoggedOut/Gallery/GalleryPage';
 
 function App() {
-  const {token} = useContext(AuthContext);
+  // const {token} = useContext(AuthContext);
 
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
+        <Navigation/>
         <Switch>
-            {token !== null? <Redirect to="/u/landing"/> : 
+            {/* {token !== null? <Redirect to="/u/landing"/> :  */}
+            
+            {/* } */}
             <Route exact path="/">
-              <Navigation/>
               <Home/>
-            </Route>}
+            </Route>
             <Route exact path="/gallery">
-              <Navigation/>
               <GalleryPage/>
             </Route>
             <Route exact path="/activities">
-              <Navigation/>
               <Activities/>
             </Route>
+            <Route exact path="/activities/form">
+              <ActivityForm/>
+            </Route>
             <Route exact path="/events">
-              <Navigation/>
               <Events/>
             </Route>
             <Route exact path="/events/eventid">
-              <Navigation/>
               <EventPage/>
             </Route>
             <Route exact path="/events/eventid/register">
-              <Navigation/>
               <EventRegister/>
             </Route>
             <Route exact path='/login'>
-              <Navigation/>
               <Login/>
             </Route>
             <Route exact path="/register">
-              <Navigation/>
               <Register/>
             </Route>
             <Route exact path="/verify">
@@ -62,11 +61,10 @@ function App() {
               <EmailVerified/>
             </Route>
         </Switch>
-        <Route path="/u"> 
+        {/* <Route path="/u"> 
           <LoggedIn/>
-        </Route>
-          
-      </Router>
+        </Route> */}
+      </HashRouter>
     </div>
   );
 }
