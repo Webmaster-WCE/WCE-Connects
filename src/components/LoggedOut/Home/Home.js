29b7bcc0641efce920a80505a9b077e0d16d3c39
cognitive-ninja <saurabhhirugade@gmail.com>
2021-11-23 09:38:32 +0530

    Replaced <a> with <Link>

--- src/components/LoggedOut/Home/Home.js
+++ src/components/LoggedOut/Home/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Footer from '../Footer/Footer';
 import WaveBorder from "./WaveBorder";
+import {Link} from 'react-router-dom';
+
 export default function Home(){
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
     return(
         <>
+            {/* {PF+"wce_pic.jpg"} */}
             <div style={{backgroundColor:"black", color:"white"}}>
-                <img src={PF+"wce_pic.jpg"} alt="pic" style={{minWidth:"-webkit-fill-available",maxWidth:"-webkit-fill-available", maxHeight:"460px"}}/>
+                <img src={PF+"/wce_pic.jpg"} alt="WCE_IMAGE" style={{minWidth:"-webkit-fill-available",maxWidth:"-webkit-fill-available", maxHeight:"460px"}}/>
             </div>
             <WaveBorder
                 upperColor="#000000"
@@ -14,12 +20,12 @@ export default function Home(){
                 // className={classes.waveBorder}
                 animationNegativeDelay={2}
             />
-            <div style={{height:"60vh"}}>
+            <div style={{height:"70vh"}}>
                 <div>
-                    <p style={{fontSize:"large", fontWeight:"bold", fontFamily:"Montserrat"}}>Featured Events</p>
-                    <a href="/events/eventid" style={{backgroundColor:"white"}}><img src={PF+"event_banner.png"} alt="pic" style={{ maxHeight:"250px", padding:"30px"}}/></a>
+                    <p style={{fontSize:"x-large", fontWeight:"bold", fontFamily:"Montserrat"}}>Featured Events</p>
+                    <Link to="/events/eventid" style={{backgroundColor:"white", textDecoration: "none"}}><img src={PF+"/event_banner.jpg"} alt="EVENT_BANNER" style={{ maxHeight:"250px", padding:"30px"}}/></Link>
                 </div>
-                <a href="/events/" style={{fontSize:"large", fontWeight:"bold", fontFamily:"Montserrat"}}>View All Events</a>
+                <Link to="/events/" style={{ textDecoration:"none"}}><p style={{fontSize:"x-large", fontWeight:"bold", fontFamily:"Montserrat"}}>View All Events</p></Link>
             </div>
             <Footer/>
         </>
