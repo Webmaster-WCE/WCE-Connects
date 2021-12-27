58ad7b9f540e28e319270e923ae7e18f005f8fa0
rushelmac <rushishelke633@gmail.com>
2021-12-27 19:41:27 +0530

    Refactor. Changed 'var' to 'const'.

--- server/controllers/excel-to-json.js
+++ server/controllers/excel-to-json.js
@@ -1,26 +1,24 @@
-var XLSX=require('xlsx');
+const XLSX=require('xlsx');
 
-var data=function excelFileTojson (FileName) {
-    var workbook=XLSX.readFile(FileName);
-    var sheet_name_list = workbook.SheetNames;
+const data=function excelFileTojson (FileName) {
+    const workbook=XLSX.readFile(FileName);
+    const sheet_name_list = workbook.SheetNames;
     console.log(sheet_name_list); // getting as Sheet1
     sheet_name_list.forEach(function (y) {
-    var worksheet = workbook.Sheets[y];
-    //getting the complete sheet
-    // console.log(worksheet);
+    const worksheet = workbook.Sheets[y];
     
-    var headers = {};
-    var data = [];
+    const headers = {};
+    const data = [];
     for (z in worksheet) {
      if (z[0] === "!") continue;
      //parse out the column, row, and value
-     var col = z.substring(0, 1);
+     const col = z.substring(0, 1);
      // console.log(col);
  
-     var row = parseInt(z.substring(1));
+     const row = parseInt(z.substring(1));
      // console.log(row);
  
-     var value = worksheet[z].v;
+     const value = worksheet[z].v;
      // console.log(value);
  
      //store header names
