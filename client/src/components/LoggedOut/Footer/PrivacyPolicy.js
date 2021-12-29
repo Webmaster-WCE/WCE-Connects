5c5ab18e6c359e05056cd78e090a6bc37c8da521
cognitive-ninja <saurabhhirugade@gmail.com>
2021-12-29 22:57:23 +0530

    Added privacy policy

---
+++ client/src/components/LoggedOut/Footer/PrivacyPolicy.js
@@ -0,0 +1,192 @@
+import React,{ useEffect } from 'react'
+
+const PrivacyPolicy = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    },[])
+
+    return (
+        <>
+            <div 
+                style={{ 
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    marginTop: "5.5%" 
+                }} 
+                className='container'
+            >
+                <div>
+                    <p 
+                        style={{ fontSize: "x-large", fontWeight: "bold", fontFamily: "Montserrat" }}
+                    >
+                        Privacy Policy
+                    </p>
+                </div>
+                <div 
+                    style={{ 
+                        textAlign: "left", 
+                        marginBottom: "50px", 
+                        borderRadius: "50px", 
+                        padding: '30px', 
+                        fontFamily: "Montserrat", 
+                        maxWidth: "700px", 
+                        width:"80%",
+                        backgroundColor: "#ededed" ,
+                        
+                    }}
+                >
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            1. General Statement
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        The Walchand College of Engineering, Sangli (“Walchand COE”, “WCE”, “we”, “our”) respects your privacy and we are committed to protecting it. This Privacy Statement explains our policies and practices regarding the information we collect from and about you. It is through this Privacy Statement that we intend to provide you with a level of comfort and confidence in how we collect, use, and safeguard personal and other information that we collect and obtain, or that you provide through this website, and how you can contact us if you have any questions or concerns. It is our sincere hope that by explaining our data handling practices we will develop a trusting and long-lasting relationship with you. By using this website, you agree to the terms of this Privacy Statement.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            2. Information about  Walchand College of Engineering, Sangli  and their Website
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        This Privacy Statement applies to the website located at https://www.alumni.wce.ac.in/#/activities/form, its related mobile website, and its applications (collectively, “Site”), which are owned and operated by the Department of Computer Science and Engineering. The purpose of this Site is to provide visitors with:  
+                        <ul style={{listStyle:"none"}}>
+                            <li>
+                                (1) Information about the Platinum Jubilee meet 
+                            </li>
+                            <li>
+                                (2) To collect and assist with the registration of the Alumni for the meet.                    
+                            </li>
+                            <li>
+                                (3) To update information regarding the upcoming events of the Platinum jubilee year; 
+                            </li>
+                            <li>
+                                (4) The permission to update your contact information and communication preferences on your behalf;
+                            </li>
+                            <li>
+                                (5) An avenue to share your story and/or feedback regarding Platinum Jubilee; and 
+                            </li>
+                            <li>
+                                (6) To get information about the willingness of participation of the alumni in various activities.
+                            </li>
+                        </ul>
+                        
+                        The Walchand College of Engineering makes every effort to ensure that the information available through and the functionality of the Site is consistent and accessible across all devices; however, some features of the Site are better accessed through non-mobile devices and vice versa.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            3. Information We Collect From and About You
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                    Information You Provide to the Walchand College of Engineering. For purposes of this Privacy Statement, “personally identifiable information” is information that identifies you as an individual, (e.g., your name, street address, telephone number, e-mail address, and/or billing information, your current post, company). When you visit the Site, we will not collect any personally identifiable information about you unless you provide it to us voluntarily. If you opt not to provide us with personally identifiable information, you can still access the Site; however, some functionalities of the Site may not be available to you.
+                    The Site may collect personally identifiable information from you when, for example, you join or update your information, donate on the website, and if you participate in or post about certain Site activities.
+                    <br/>
+                    If you transact with us, we collect some additional information, such as a billing address, a credit/debit card number, and a credit/debit card expiration date, and/ or other payment instrument details. If you send us personal correspondence, such as emails or letters, or if other users or third parties send us correspondence about your activities or postings on the Website, we may collect such information into a file specific to you. We collect personally identifiable information (email address, name, phone number, credit card/debit card / other payment instrument details, etc.) from you when you set up, provide such information to us. While you can browse some sections of our Website without providing such information, certain activities do require such information from us. We do use your contact information to send you offers based on your previous orders and your interests.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            4. How the Walchand College of Engineering  Uses Your Information
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        The Walchand College of Engineering may use the personally identifiable and non-personally identifiable information collected from and about you to:
+                        Fulfill Site user requests (e.g., creating or updating any log-in credentials; the sending of updates regarding the Platinum jubilee including e-mails, invitations, messages.)
+                        <ul>
+                            <li>
+                                Responding to your comments or requests for information
+                            </li>
+                            <li>
+                                Contact you for updates, confirmation of registration, or communications from the Walchand College of Engineering.
+                            </li>
+                            <li>
+                                Tracking, measuring, and analyzing your use of and activity relating to the Site and your engagement with the Foundation, our programs, services, and activities to enhance the Institute the relationship with you
+                            </li>
+                            <li>
+                                Fulfilling a request for or to develop new products or services before Generating analytics that improves our Site layout, content, offerings, and services
+                            </li>
+                            <li>
+                                Complying with legal requirements
+                            </li>
+                            <li>
+                                Compiling aggregate and statistical data to help in website design and to identify popular features
+                            </li>
+                            <li>
+                                Measuring site activity to allow us to update our Site to better meet user wants and needs.
+                            </li>
+                        </ul>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            5. Sharing Your Personally Identifiable Information
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        Alumni and User data are the property of the Walchand College of Engineering, which has the final authority over decisions about sharing your information. The Institute does not share, sell, or rent personally identifiable information with unaffiliated entities for their use without obtaining your permission first.
+                        Personally identifiable information that you provide to us while using Site features or requesting registration or service through this Site may be stored in one or more of our databases.
+                        Service providers, contractors, or subcontractors that have access to your personally identifiable information in connection with providing services for this Site are required to keep your information confidential are not permitted to use this information for any other purpose than to fulfill the services that they are providing for the Institute or the Site.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            6. Access To and Control Over Your Personally Identifiable Information
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        To correct, update, or delete your personally identifiable information maintained by the Walchand College of Engineering, you may contact us through any of the options listed in Section below. Please supply your name, address, and specify what information you would like to correct, update, or withhold so that we may process your request. We will try to comply with your request as soon as reasonably possible.
+                        The scope of access available for this request is for information stored in the database that holds the data for this Site. Any personally identifiable information that has been collected from you will not be accessible or changeable through the processing of this request.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            7. Security of Your Information
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        Safeguarding the information of visitors to our Site is of the utmost importance to us. We use reasonable administrative, technical, personnel, and physical steps to maintain the accuracy of the information and to protect your information from loss, unauthorized usage, and/or alteration.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            8. Consent to Process
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        By using the Website www.alumni.wce.ac.in and/ or by providing your information, you consent to the collection and use of the information you disclose on the Website following this Privacy Policy, including but not limited to your consent for sharing your information as per this privacy policy. If we decide to change our privacy policy, we will post those changes on this page so that you are always aware of what information we collect, how we use it, and under what circumstances we disclose it.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            9. Links to Other Sites
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        This Site provides links to other websites including. We encourage you to review the privacy statements of all sites which you visit, including those whose links are provided on this Site, so that you can understand how those sites collect, use, and share your information. This Site is not responsible for the privacy statements, content, or data handling practices on other websites.
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            10. Grievance Officer
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        Following the Information Technology Act, 2000 and rules made thereunder, the name and contact details of the Grievance Officer are provided below: 
+                        Name: Dr. M. A. Shah
+                        Email: hod.cse@walchandsangli.ac.in
+                        Mobile no: +91 94238 72296
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        <strong>
+                            11. Revisions to this Privacy Statement
+                        </strong>
+                    </p>
+                    <p style={{ textAlign: "left" }}>
+                        Occasionally, we may update this Privacy Statement in order to reflect any changes to the Site and/or our privacy practices. If we make any updates, the new Privacy Statement will be posted to the website 10 days prior to the changes taking effect. 
+                        <br/>
+                        <br/>
+                        Copywritten by: Sanraj Gandhi and Atique Pathan
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PrivacyPolicy
