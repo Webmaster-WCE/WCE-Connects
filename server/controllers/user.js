42a2249716f50e1b2a6425b90845bb99b610c628
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2022-02-10 01:28:47 +0530

    Add: signin and signup routes with bcrypt and jwt

---
+++ server/controllers/user.js
@@ -0,0 +1,53 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const UserModal = require("../models/user.js");
+
+const secret = process.env.SECRETE;
+
+async function signin (req, res){
+  const { email, password } = req.body;
+
+  try {
+    const oldUser = await UserModal.findOne({ email });
+
+    if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
+
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+async function signup (req, res) {
+  const { email, password, department, role, name } = req.body;
+
+  try {
+    const oldUser = await UserModal.findOne({ email });
+
+    if (oldUser) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await UserModal.create({ email, password: hashedPassword, name, department, role });
+
+    const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
+
+    res.status(201).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    
+    console.log(error);
+  }
+};
+
+module.exports = {
+  signin,
+  signup
+}
