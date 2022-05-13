a020cbf637d33907ea5656a7a115754b8973b092
“OnkarGavali” <onkargavali1561@gmail.com>
2022-05-13 13:34:35 +0530

    graduation day schedule update

--- client/src/components/LoggedOut/Events/Graduation/Graduation.js
+++ client/src/components/LoggedOut/Events/Graduation/Graduation.js
@@ -63,23 +63,23 @@ export default function Graduation() {
     }
     const Schedule = [
         createSceduleData("3:30 pm", "Arrival of VC","10 min"),
-        createSceduleData("3:40 pm", "Exam Section Inauguration","5 min"),
+        createSceduleData("3:40 pm", "Inauguration of New facilities at Exam Section and Projection of Virtual GDC link","5 min"),
         createSceduleData("3:45 pm", "Exam Section Video with Tea & Snacks","10 min"),
         createSceduleData("3:55 pm", "Information about Exam Section Processes","10 min"),
-        createSceduleData("4:05 pm", "Tree Plantation in Exam Section Premises","10 min"),
-        createSceduleData("4:15 pm", "@ Tilak Hall for GDC Program","5 min"),
-        createSceduleData("4:20 pm", "Saraswati Vandana","5 min"),
-        createSceduleData("4:25 pm", "Welcome and Felicitation of VC","5 min"),
-        createSceduleData("4:30 pm", "Welcome Address by Director","10 min"),
-        createSceduleData("4:40 pm", "Address by Chairman (video)","15 min"),
-        createSceduleData("4:55 pm", "Address by Mashelkar sir (video)","25 min"),
-        createSceduleData("5:20 pm", "Address by VC","10 min"),
-        createSceduleData("5:30 pm", "Award Ceremony","35 min"),
-        createSceduleData("6:05 pm", "Student Feedback","10 min"),
-        createSceduleData("6:15 pm", "Vote of Thanks","5 min"),
-        createSceduleData("6:25 pm", "National Anthem","5 min"),
-        createSceduleData("6:30 pm", "High Tea at Saraswati Idol premises","30 min"),
-        createSceduleData("7:00 pm", "Cultural event @ Tilak Hall","")
+        createSceduleData("4:05 pm", "@ Tilak Hall for GDC Program","5 min"),
+        createSceduleData("4:10 pm", "Saraswati Vandana ","5 min"),
+        createSceduleData("4:15 pm", "Welcome and Felicitation of VC","5 min"),
+        createSceduleData("4:20 pm", "Welcome Address by Director","5 min"),
+        createSceduleData("4:25 pm", "Address by Chairman, Shri. Ajitji Gulabchand (video)","15 min"),
+        createSceduleData("4:40 pm", "Address by Dr. Raghunath Mashelkar (video) ","25 min"),
+        createSceduleData("5:05 pm", "Address by VC","10 min"),
+        createSceduleData("5:15 pm", "Award Ceremony","35 min"),
+        createSceduleData("5:50 pm", "Student Feedback","10 min"),
+        createSceduleData("6:00 pm", "Vote of Thanks","3 min"),
+        createSceduleData("6:03 pm", "National Anthem","5 min"),
+        createSceduleData("6:08 pm", "Tree Plantation in Exam Section Premises","10 min"),
+        createSceduleData("6:18 pm", "High Tea at Saraswati Idol premises","30 min"),
+        createSceduleData("6:48 pm", "Cultural event @ Library Staircase","")
         
     ];
 
@@ -110,7 +110,7 @@ export default function Graduation() {
                         <div style={{  }} className={classes.abc}>
                             {/* <p style={{ fontSize: "large", fontWeight: "bold", fontFamily: "Montserrat" }}>EVENT DAY: 8th Jan 2022</p>
                             <p style={{ fontSize: "large", fontWeight: "bold", fontFamily: "Montserrat" }}>EVENT TIME: 10:00 AM</p> */}
-                            <p style={{ fontSize: "large", fontWeight: "bold", fontFamily: "Montserrat" }}>Date & Time : 17<sup>th</sup> May at 3.00 pm</p>
+                            <p style={{ fontSize: "large", fontWeight: "bold", fontFamily: "Montserrat" }}>Date & Time : 17<sup>th</sup> May at 3.3 0 pm</p>
                             <p style={{ fontSize: "large", fontWeight: "bold", fontFamily: "Montserrat" }}>Venue: Tilak Hall</p>
                             {/* <p style={{ fontSize: "large", fontWeight: "bold", fontFamily: "Montserrat" }}>Date & Time : <span style={{color:"#8793ff"}}>Projection of the event is postponed</span>  </p> */}
                             <p style={{fontWeight: "bold", fontFamily: "Montserrat" }}><a href={"https://youtu.be/9ZQUigvicBg"} target="_blank"> Glimpses of GDC</a></p>
@@ -120,35 +120,8 @@ export default function Graduation() {
                     <Grid item xs={12} sm={6}>
                         <div style={{  }} className={classes.xyz} >
                             <div>
-                                {/* <Link to="/events/eventid/preregistration" style={{ textDecoration: "none" }}>
-                                    <Button
-                                        type="submit"
-                                        variant="contained"
-                                        style={{ background: 'lightgrey' }}
-                                    >
-                                        <div style={{ color: "blue", fontSize: "medium" }}>
-                                            REGISTER NOW
-                                        </div>
-                                    </Button>
-                                </Link> */}
-
-                                {/* link is disabled */}
-                                
-                                {/* <Button
-                                    type="button"
-                                    variant="contained"
-                                    style={{ background: 'lightgrey' }}
-                                    onClick={()=> alert("The Registration has been closed")}
-                                >
-                                    <div style={{ color: "blue", fontSize: "medium" }}>
-                                        REGISTER NOW
-                                    </div>
-                                </Button>
-                                
-
-                                <p><small style={{color:'red'}}>Registration link will be closed by 4th January 2022, 5.00 pm IST</small></p> */}
-                                <p style={{fontWeight: "bold", fontFamily: "Montserrat" }}>Link to view Virtual GDC: <a href={"https://youtu.be/VXQqI9gtwrs" }className="blink_me" target="_blank">click here</a></p>
-                                 <p><small style={{color:'red'}}>Content under this link will be publicly available after 4pm on 17th May.</small></p>
+                              <p style={{fontWeight: "bold", fontFamily: "Montserrat" }}>Link to view Virtual GDC: <a href={"https://youtu.be/VXQqI9gtwrs" }className="blink_me" target="_blank">click here</a></p>
+                                 <p><small>Content under this link will be publicly available after 4pm on 17th May.</small></p>
                             
                             </div>
                             
@@ -177,6 +150,8 @@ export default function Graduation() {
                 <p style={{ fontSize: "xx-large", fontWeight: "bold", paddingTop: "1%" }}>Invocation</p>
                 <p style={{ fontSize: "x-large", backgroundColor: "#e6e6e6", padding: "5%", margin: "1% 10%", textAlign: "left", lineHeight: "1.5", borderRadius: "30px" }}>
                     The 10th Graduation Day Ceremony link projection and Award distribution function is scheduled on 17th May 2022 in the presence of Vice-chancellor, Shivaji University Kolhapur at the same time inauguration of New Exam Section facilities is planned. For this program Award winning students with max two family members and our faculties are invited. All invitees are requested to follow COVID government guidelines during the program.
+                    <br/><br/>
+                    Guest: Dr. D. T. Shirke, Vice-Chancellor, Shivaji University, Kolhapur
                 </p>
                
             </div>
